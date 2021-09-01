8af850d701f8f3239935f0193becfe178e99440e
CherevatkinDima <80575314+CherevatkinDima@users.noreply.github.com>
2021-09-01 12:55:14 +0300

    add spark

---
+++ spark/js/main.js
@@ -0,0 +1,158 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('.header__link').click(function(event) {
+        $('.header__burger,.header__menu').removeClass('active');
+        $('body').removeClass('lock');
+    });
+
+
+
+
+
+
+    $('.popup-link').click(function() {
+        
+        
+        $('body').addClass('lock');
+        $('.popup').fadeIn();
+        event.preventDefault();
+        
+
+    
+    });
+
+    $('.popup__close').click(function() {
+    
+        $('.popup').fadeOut();
+        $('body').toggleClass('lock');
+        event.preventDefault();
+    
+    });
+
+
+    $("body").on('click', '[href*="#"]', function(e){
+    var fixed_offset = 0;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+    });
+
+    
+
+
+    $("#phone").mask("+7  (999) 999-9999");
+
+    $('.slider').slick({
+        swipe:true
+    });
+
+
+
+    
+
+    $('#tamb').click(function() {
+
+        $('*').removeClass('active');
+       
+        
+        $('#1-1').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#vhod').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#1-2').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#fire').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#1-3').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#gates').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#2-1').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#fance').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#2-2').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#nerj').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#2-3').addClass('active');
+        event.preventDefault();
+    
+    });
+
+    $('#stairs').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#2-4').addClass('active');
+        event.preventDefault();
+    
+    });
+    
+    $('#canopy').click(function() {
+
+        $('*').removeClass('active');
+        
+        
+        $('#2-5').addClass('active');
+        event.preventDefault();
+    
+    });
+
+
+    
+
+    $('.catalog__burger').click(function(event) {
+        $('.catalog__burger,.catalog__left').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+
+    $('li').click(function(event) {
+        $('.catalog__burger,.catalog__left').removeClass('active');
+        $('body').removeClass('lock');
+    });
+
+
+});
+
+
+
+
+
+
+
+
